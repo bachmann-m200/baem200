28375e0ffc5c611971da628f584c4faf05e687e5
Sven Neumann <seven.newman@icloud.com>
2022-05-12 23:25:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="baem200",
-    version="0.0.11",
+    version="0.0.12",
     author="Sven Neumann",
-    author_email="sven.neumann@web.de",
+    author_email="sven.neumann@bachmann.info",
     description="Bachmann electronic Python Util Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bachmann-m200/baem200",
-	data_files=[("DLLs/baem200/x64", ["x64/m1com64.dll", "x64/log.prp"])],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
