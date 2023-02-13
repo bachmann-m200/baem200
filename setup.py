c0ff7f4ac80836ed0c9812e71c9db23af5206d57
Sven NEUMANN <Sven.NEUMANN@bachmann.info>
2023-02-13 11:36:54 +0100

    change version for PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="baem200",
-    version="0.0.12",
+    version="0.0.13",
     author="Sven Neumann",
     author_email="sven.neumann@bachmann.info",
     description="Bachmann electronic Python Util Library",
